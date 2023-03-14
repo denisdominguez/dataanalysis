d862fd67bf56d787f0fe658d46ce95021f02a4b0
Denis <denis@dominguez.de>
2023-03-14 02:08:52 +0100

    Adding the DataController

--- src/main/java/com/tempo/dataanalysis/controllers/DataController.java
+++ src/main/java/com/tempo/dataanalysis/controllers/DataController.java
@@ -1,2 +1,11 @@
-package com.tempo.dataanalysis.controllers;public class DataController {
+package com.tempo.dataanalysis.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DataController {
+
+    public String getData() {
+        return "index";
+    }
 }
